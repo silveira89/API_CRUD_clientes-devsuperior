8836648ff8ee5a00873831ec3836591a85fe3088
Andre Silveira <alasandre@gmail.com>
2021-11-22 09:54:42 -0300

    Created CRUD

---
+++ backend/src/main/java/com/devsuperior/API_CRUD_clientes/ApiCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.API_CRUD_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudClientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/API_CRUD_clientes/ApiCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.API_CRUD_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
